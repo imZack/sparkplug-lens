da316e9556b6f3eb898844921bc680cab4ed919a
Zack Shih <shih@yulun.me>
2024-04-15 13:43:19 +0800

    refactor: update

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/imZack/sparkplug-lens
 
-go 1.20
+go 1.22
 
 require github.com/eclipse/paho.mqtt.golang v1.4.3
 
@@ -8,13 +8,14 @@ require (
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
 
 require (
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	google.golang.org/protobuf v1.33.0
 )
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,42 +9,55 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/hokaccha/go-prettyjson"
 	"github.com/imZack/sparkplug-lens/internal/spb"
+	log "github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/proto"
 )
 
-var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("-------- %s -------- (%d bytes) \n", msg.Topic(), len(msg.Payload()))
-	spbPayload := &spb.Payload{}
-	if err := proto.Unmarshal(msg.Payload(), spbPayload); err == nil {
-		if jsonString, err := prettyjson.Marshal(spbPayload); err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(string(jsonString))
+func DecodeAsSPB(msg mqtt.Message) (*spb.Payload, error) {
+	payload := &spb.Payload{}
+	if err := proto.Unmarshal(msg.Payload(), payload); err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
+
+var f mqtt.MessageHandler = func(_ mqtt.Client, msg mqtt.Message) {
+	var jsonString []byte
+	var spbPayload *spb.Payload
+	if spbPayload, err := DecodeAsSPB(msg); err == nil {
+		// 1. Try decode as SPB payload -> Print as JSON
+		jsonString, err = prettyjson.Marshal(spbPayload)
+		if err != nil {
+			log.Info("error", err)
+			return
 		}
-		fmt.Println("======================================")
-		return
 	} else {
-		// Can't decode spb payload, try to decode as JSON Object
-		// Ignore error
+		// 2. Try decode as JSON payload
+		if jsonString, err = prettyjson.Format(msg.Payload()); err != nil {
+			log.Info("error", err)
+			return
+		}
 	}
 
-	// Check if msg.Payload() is a JSON string
-	if jsonString, err := prettyjson.Format(msg.Payload()); err == nil {
-		fmt.Println(string(jsonString))
-	} else {
-		fmt.Println(err)
-	}
-
-	fmt.Println("======================================")
+	log.WithFields(log.Fields{
+		"topic": msg.Topic(),
+		"size":  len(msg.Payload()),
+		"isSPB": spbPayload != nil,
+	}).Info(string(jsonString))
 }
 
 func main() {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
 	brokerEndpoint := os.Getenv("MQTT_BROKER")
 	if brokerEndpoint == "" {
 		brokerEndpoint = "tcp://test.mosquitto.org:1883"
-		fmt.Println("Using default broker endpoint: ", brokerEndpoint)
+		log.Info("Using default broker endpoint: ", brokerEndpoint)
 	} else {
-		fmt.Println("Using broker endpoint: ", brokerEndpoint)
+		log.Info("Using broker endpoint: ", brokerEndpoint)
 	}
 
 	opts := mqtt.NewClientOptions().
@@ -54,34 +66,34 @@ func main() {
 		SetDefaultPublishHandler(f).
 		SetAutoReconnect(true).
 		SetConnectionLostHandler(func(client mqtt.Client, err error) {
-			fmt.Println("[SetConnectionLostHandler] Connection lost: ", err)
+			log.Warn("[SetConnectionLostHandler] Connection lost: ", err)
 		}).
-		SetOnConnectHandler(func(client mqtt.Client) {
-			fmt.Println("[SetOnConnectHandler] Connected")
+		SetOnConnectHandler(func(_ mqtt.Client) {
+			log.Info("[SetOnConnectHandler] Connected")
 		})
 
 	if os.Getenv("MQTT_USERNAME") != "" {
-		fmt.Println("Using username: ", os.Getenv("MQTT_USERNAME"))
+		log.Info("Using username: ", os.Getenv("MQTT_USERNAME"))
 		opts.SetUsername(os.Getenv("MQTT_USERNAME"))
 	}
 
 	if os.Getenv("MQTT_PASSWORD") != "" {
-		fmt.Println("Using password: **********")
+		log.Info("Using password: **********")
 		opts.SetPassword(os.Getenv("MQTT_PASSWORD"))
 	}
 
 	c := mqtt.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
+		log.Panic(token.Error())
 	}
 
 	if token := c.Subscribe("spBv1.0/#", 0, nil); token.Wait() && token.Error() != nil {
-		fmt.Println(token.Error())
+		log.Error(token.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println("Start listening...")
-	fmt.Println("Press Ctrl+C to exit")
+	log.Info("Start listening...")
+	log.Info("Press Ctrl+C to exit")
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
